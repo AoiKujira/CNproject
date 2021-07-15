2cf22a9249546b7337d24abe3dc9f6bb7579e3b7
Far <farzamzohdi@gmail.com>
2021-07-15 18:17:52 +0430

    minor change

--- Messenger.py
+++ Messenger.py
@@ -1,15 +1,17 @@
+import Packet
+
 class Messenger:
 
-    def decode_message(message):
+    def decode_packet(message: str):
         try:
             message = message.split(maxsplit=3)
-            return int(message[0]), int(message[1]), int(message[2]), str(message[3])
+            return Packet(int(message[0]), int(message[1]), int(message[2]), str(message[3]))
         except Exception as e:
             print(e)
 
-    def encode_message(message_type, source_id, destination_id, data):
-        message = str(message_type) + '\n' \
-                + str(source_id) + '\n' \
-                + str(destination_id) + "\n" \
-                + str(data)
+    def encode_packet(packet: Packet):
+        message = str(packet.message_type) + '\n' \
+                + str(packet.source_id) + '\n' \
+                + str(packet.destination_id) + "\n" \
+                + str(packet.data)
         return message

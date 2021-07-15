9900bb47d8c86e9c47c19c16468454f75757ae8f
amirmohammad ghasemi <ghasemiamirmohammad@yahoo.com>
2021-07-15 14:07:39 +0430

    Conpleted PeerConnector.

---
+++ Address.py
@@ -0,0 +1,6 @@
+class Address:
+
+    def __init__(self, host: str, port: int, identifier: int):
+        self.host = host
+        self.port = port
+        self.id = identifier
--- Peer/PeerConnector.py
+++ Peer/PeerConnector.py
@@ -1,33 +1,37 @@
 import socket as so
+import re
 from Configuration import *
+from Address import Address
+
+request_message = 'CONNECT AS {identifier} ON PORT {port}'
+success_response = 'CONNECT TO (\\d+) WITH PORT (\\d+)'
 
 
 class PeerConnector:
 
-    def __init__(self, identifier, port):
-        self.id = identifier
-        self.port = port
-
-        self.socket = None
-        self.socket: so.socket
+    def __init__(self, address: Address):
+        self.address = address
+        self.socket = so.socket(so.AF_INET, so.SOCK_STREAM)
 
     def connect(self):
-        self.socket = so.socket(so.AF_INET, so.SOCK_STREAM)
         self.socket.connect((MANAGER_HOST, MANAGER_PORT))
-        self.socket.recv(BUFFER_SIZE).decode(ENCODING)
 
-    def send_connection_message(self):
-        s = f'CONNECT AS {self.id} ON PORT {self.port}'
-        self.socket.send(s.encode(ENCODING))
+    def send_connection_request(self):
+        request = request_message.encode(str(self.address.id), str(self.address.port))
+        self.socket.send(request.encode(ENCODING))
+        return self.socket.recv(BUFFER_SIZE).decode(ENCODING)
 
-    def receive_message(self):
-        self.socket.recv()
+    def handle(self):
+        response = self.send_connection_request()
+        x = re.match(response, success_response)
+        if x is not None:
+            parent_id = int(x.group(1))
+            parent_port = int(x.group(2))
+            parent_host = MANAGER_PORT
+            return Address(parent_host, parent_port, parent_id)
+        else:
+            raise Exception("failed")
 
     def get_id(self):
         self.connect()
-        self.send_connection_message()
-        self.socket.
-
-
-
-
+        return self.handle()

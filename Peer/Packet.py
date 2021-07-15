b676bd293c7772770c51ac11451bb6f49d7cfdf8
amirmohammad ghasemi <ghasemiamirmohammad@yahoo.com>
2021-07-15 13:10:23 +0430

    Added Packet class.

---
+++ Peer/Packet.py
@@ -0,0 +1,10 @@
+from Peer.PacketType import PacketType
+
+
+class Packet:
+
+    def __init__(self, *, packet_type: PacketType, source_id: int, destination_id: int, data: str):
+        self.type = packet_type
+        self.source_id = source_id
+        self.destination_id = destination_id
+        self.data = data

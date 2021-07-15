9a9822e8312ade5d25899603420efb2d4b865f0e
amirmohammad ghasemi <ghasemiamirmohammad@yahoo.com>
2021-07-15 18:22:16 +0430

    WIP

--- Messenger.py
+++ Messenger.py
@@ -1,17 +1,17 @@
 import Packet
 
-class Messenger:
 
-    def decode_packet(message: str):
-        try:
-            message = message.split(maxsplit=3)
-            return Packet(int(message[0]), int(message[1]), int(message[2]), str(message[3]))
-        except Exception as e:
-            print(e)
+def decode_packet(message: str):
+    try:
+        message = message.split(maxsplit=3)
+        return Packet(int(message[0]), int(message[1]), int(message[2]), str(message[3]))
+    except Exception as e:
+        print(e)
 
-    def encode_packet(packet: Packet):
-        message = str(packet.message_type) + '\n' \
-                + str(packet.source_id) + '\n' \
-                + str(packet.destination_id) + "\n" \
-                + str(packet.data)
-        return message
+
+def encode_packet(packet: Packet):
+    message = str(packet.message_type) + '\n' \
+              + str(packet.source_id) + '\n' \
+              + str(packet.destination_id) + "\n" \
+              + str(packet.data)
+    return message
--- Peer.py
+++ Peer.py
@@ -2,15 +2,21 @@ from Util import *
 from Address import Address
 from Configuration import *
 from PeerConnector import PeerConnector
+from Packet import Packet
 import time
 import socket as so
 import threading
+from PacketType import PacketType
+from Child import Child
+from Messenger import *
 
 
 class Peer:
 
     def __init__(self):
         self.address, self.parent_address = self.connect_to_network()
+        self.parent_socket = None
+        self.connect_to_parent()
 
     def connect_to_network(self) -> (Address, Address):
         while True:
@@ -35,9 +41,44 @@ class Peer:
 
         while True:
             client, address = server.accept()
-            threading.Thread(target=self.handle_client, args=client).run()
+            child = Child(client)
+            threading.Thread(target=self.listen_to_child, args=(child,)).run()
 
-    def handle_client(self, client: so.socket):
+    def connect_to_parent(self):
+        self.parent_socket = so.socket(so.AF_INET, so.SOCK_STREAM)
+        self.parent_socket.connect((self.parent_address.host, self.parent_address.port))
+
+    def advertise_to_parent(self):
+        pass
+
+    def listen_to_child(self, child: Child):
+        while True:
+            s = child.socket.recv(BUFFER_SIZE).decode(ENCODING)
+            packet = decode_packet(s)
+            self.handle_message(packet)
+        pass
+
+    def handle_message(self, packet: Packet):
+        if packet.type == PacketType.MESSAGE:
+            pass
+        elif packet.type == PacketType.ROUTING_REQUEST:
+            pass
+        elif packet.type == PacketType.ROUTING_RESPONSE:
+            pass
+        elif packet.type == PacketType.PARENT_ADVERTISE:
+            pass
+        elif packet.type == PacketType.ADVERTISE:
+            pass
+        elif packet.type == PacketType.DESTINATION_NOT_FOUND_MESSAGE:
+            pass
+        elif packet.type == PacketType.CONNECTION_REQUEST:
+            pass
+        else:
+            raise Exception("NOT SUPPORTED PACKET TYPE")
+
+    def send_message_to_socket(self, socket: so.socket, packet: Packet) -> None:
+        s = encode_packet(packet)
+        socket.send(s.encode(ENCODING))
         pass
 
 

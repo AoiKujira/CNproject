d2c96937141ab4b8601ba4b95219ffd180d9e79c
amirmohammad ghasemi <ghasemiamirmohammad@yahoo.com>
2021-07-15 14:25:54 +0430

    WIP

--- Peer/Peer.py
+++ Peer/Peer.py
@@ -1,3 +1,10 @@
+from PortAssigner import get_random_port
+
 class Peer:
 
+    def connect_to_network(self):
+        while True:
+
+
     def __init__(self):
+        self.parent_address = None
--- Peer/PeerConnector.py
+++ Peer/PeerConnector.py
@@ -3,26 +3,22 @@ import re
 from Configuration import *
 from Address import Address
 
-request_message = 'CONNECT AS {identifier} ON PORT {port}'
+request_message = '{identifier} {port}'
 success_response = 'CONNECT TO (\\d+) WITH PORT (\\d+)'
 
 
 class PeerConnector:
 
-    def __init__(self, address: Address):
-        self.address = address
+    def __init__(self):
         self.socket = so.socket(so.AF_INET, so.SOCK_STREAM)
 
-    def connect(self):
-        self.socket.connect((MANAGER_HOST, MANAGER_PORT))
-
-    def send_connection_request(self):
-        request = request_message.encode(str(self.address.id), str(self.address.port))
+    def send_connection_request(self, address: Address) -> str:
+        request = request_message.encode(str(address.id), str(address.port))
         self.socket.send(request.encode(ENCODING))
         return self.socket.recv(BUFFER_SIZE).decode(ENCODING)
 
-    def handle(self):
-        response = self.send_connection_request()
+    def handle(self, address: Address) -> Address:
+        response = self.send_connection_request(address)
         x = re.match(response, success_response)
         if x is not None:
             parent_id = int(x.group(1))
@@ -32,6 +28,9 @@ class PeerConnector:
         else:
             raise Exception("failed")
 
-    def get_id(self):
-        self.connect()
-        return self.handle()
+    def get_id(self, address: Address) -> Address:
+        self.socket.connect((MANAGER_HOST, MANAGER_PORT))
+        try:
+            return self.handle(address)
+        finally:
+            self.socket.close()

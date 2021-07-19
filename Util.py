5f414e0f9b544f3b637f4258a984704fe0a37033
amirmohammad ghasemi <ghasemiamirmohammad@yahoo.com>
2021-07-19 22:28:25 +0430

    ROUTE -1

--- Util.py
+++ Util.py
@@ -1,4 +1,3 @@
-from re import S
 import Configuration
 import Packet
 from Packet import Packet
@@ -17,10 +16,13 @@ def get_random_id() -> int:
 
 def decode_packet(message: str):
     try:
-        message = message.split(maxsplit=3)
-        return Packet(packet_type=PacketType(int(message[0])), source_id=int(message[1]),
-                      destination_id=int(message[2]),
-                      data=str(message[3]))
+        message = message.split(maxsplit=4)
+        print('messs:' , message[0], message[1], message[2], message[3])
+        p = Packet(packet_type=PacketType(int(message[0])), source_id=int(message[1]),
+                   destination_id=int(message[2]),
+                   data=str(message[4]),
+                   last_node_id=int(message[3]))
+        return p
     except Exception as e:
         print(e)
 
@@ -29,12 +31,15 @@ def encode_packet(packet: Packet):
     message = str(packet.type.value) + '\n' \
               + str(packet.source_id) + '\n' \
               + str(packet.destination_id) + "\n" \
-              + str(packet.data)
+              + str(packet.last_node_id) + "\n" \
+              + str(packet.data) + "\n"
     return message
+
 
 def encode_message_packet(data: str):
     data = data.split('\n', maxsplit=1)
     return data[1]
+
 
 def make_salam_packet(source_id: int, destination_id: int):
     return make_message_packet(source_id, destination_id, f'{source_id}: Salam Salam Sad Ta Salam')
@@ -46,27 +51,14 @@ def make_message_packet(source_id: int, destination_id: int, data:str):
     return Packet(packet_type=PacketType.MESSAGE,
                   source_id=source_id,
                   destination_id=destination_id,
-                  data=data)
+                  data=data,
+                  last_node_id=source_id)
 
 def make_connection_request_packet(source_id: int, destination_id: int, port: int):
     return Packet(packet_type=PacketType.CONNECTION_REQUEST, source_id=source_id, destination_id=destination_id,
-                  data=str(port))
+                  data=str(port),
+                  last_node_id=source_id)
 
 
 def parse_advertise_data(data: str):
     return int(data)
-
-
-def make_parent_advertise_packet(source_id: int, parent_id: int, subtree_child_id) -> Packet:
-    return Packet(packet_type=PacketType.PARENT_ADVERTISE, source_id=source_id, destination_id=parent_id,
-                  data=str(subtree_child_id))
-
-
-def make_routing_response_packet(source_id: int, destination_id: int) -> Packet:
-    return Packet(packet_type=PacketType.ROUTING_RESPONSE, source_id=source_id, destination_id=destination_id,
-                  data=str(source_id))
-
-
-def make_destination_not_found_message_packet(source_id: int, destination_id: int, searched_id: int) -> Packet:
-    return Packet(packet_type=PacketType.DESTINATION_NOT_FOUND_MESSAGE, source_id=source_id,
-                  destination_id=destination_id, data='DESTINATION {} NOT FOUND'.format(searched_id))
